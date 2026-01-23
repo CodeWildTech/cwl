d7cd38740cd081d5c6e53086b4e8b2454f271353
james-pythondev <jamesandrew2705@gmail.com>
2026-01-23 09:28:04 +0530

    Fix case sensitivity in file paths

--- cw-learninghub/src/pages/testimonialSection.jsx
+++ cw-learninghub/src/pages/testimonialSection.jsx
@@ -6,9 +6,9 @@ import Stephin from "../assets/students/stephin.jpeg";
 import Ashik from "../assets/students/ashik.jpeg";
 import Nikhitha from "../assets/students/nikkitha.jpg";
 import Sandra from "../assets/students/sandra.jpg";
-// import Anandhu from "../assets/students/Anandhu.jpg";
+import Anandhu from "../assets/students/Anandhu.jpg";
 import Vandana from "../assets/students/vandana.jpg";
-import Hannah from "../assets/students/hannah.jpeg";
+import Hannah from "../assets/students/Hannah.jpeg";
 
 
 import { motion } from "framer-motion";
